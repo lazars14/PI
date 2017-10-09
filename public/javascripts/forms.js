c93d028048bc389ff6297c104b87112d5ca0d07d
zomlaaa95 <zomlaaa95@gmail.com>
2017-10-09 18:56:13 +0200

    Did some validation on delete entity

--- public/javascripts/forms.js
+++ public/javascripts/forms.js
@@ -1,5 +1,13 @@
 function setFormAction(action){
 	document.getElementById("crud").action = action;
-	alert(document.getElementById("crud").action);
 	document.getElementById("crud").submit();
+}
+
+function checkForDeletion(action, entity){
+	if(confirm("Da li ste sigurni da hocete da obrisete " + entity)){
+        setFormAction(action);
+    }
+    else{
+        return false;
+    }
 }
