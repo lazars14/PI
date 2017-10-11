f30753dfa8d929d68fe05904dd9d7a57a3c0e79b
zomlaaa95 <zomlaaa95@gmail.com>
2017-10-11 16:43:17 +0200

    Done some work on the read.html's

--- app/controllers/Magacini.java
+++ app/controllers/Magacini.java
@@ -12,24 +12,24 @@ public class Magacini extends Controller {
 	
 	public static void read(){
 		List<Magacin> magacini = Magacin.findAll();
-		// List<Preduzece> preduzeca = Preduzece.findAll();
+		List<Preduzece> preduzeca = Preduzece.findAll();
 		
-		render(magacini);
+		render(magacini, preduzeca);
 	}
 	
-	public static void create(String naziv){
-		Preduzece preduzece = Preduzece.findById(1);
-		Magacin magacin = new Magacin(naziv, preduzece);
+	public static void create(String naziv, Long preduzece){
+		Preduzece p = Preduzece.findById(preduzece);
+		Magacin magacin = new Magacin(naziv, p);
 		magacin.save();
 		
 		read();
 	}
 
-	public static void update(Long id, String naziv){
-		Preduzece preduzece = Preduzece.findById(1);
+	public static void update(Long id, String naziv, Long preduzece){
+		Preduzece p = Preduzece.findById(preduzece);
 		Magacin magacin = Magacin.findById(id);
 		magacin.naziv = naziv;
-		magacin.preduzece = preduzece;
+		magacin.preduzece = p;
 		magacin.save();
 		
 		read();
