6e08d543a6dfaea4da610ed860d7af9d3be33b0a
zomlaaa95 <zomlaaa95@gmail.com>
2017-10-17 02:17:43 +0200

    Did some work on prometnaDokumenta

--- app/controllers/Prometnadokumenta.java
+++ app/controllers/Prometnadokumenta.java
@@ -14,27 +14,36 @@ public class Prometnadokumenta extends Controller{
 	
 	public static void read(){
 		List<Prometnidokument> dokumenti = Prometnidokument.findAll();
+		List<Magacin> magacini = Magacin.findAll();
+		List<Poslovnipartner> partneri = Poslovnipartner.findAll();
 	
-		render(dokumenti);
+		render(dokumenti, magacini, partneri);
 	}
 	
-	public static void create(String vrstaDokumenta, int redniBrojDokumenta, Date datumFormiranja, Date datumKnjizenja, Character statusDokumenta, Magacin magacin, Poslovnipartner poslovnipartner){
-		Prometnidokument pd = new Prometnidokument(vrstaDokumenta, redniBrojDokumenta, datumFormiranja, datumKnjizenja, statusDokumenta, magacin, poslovnipartner);
+	public static void create(String vrstaDokumenta, int redniBrojDokumenta, Date datumFormiranja, Date datumKnjizenja,
+			Character statusDokumenta, Long magacin, Long poslovniPartner){
+		
+		Magacin m = Magacin.findById(magacin);
+		Poslovnipartner pp = Poslovnipartner.findById(poslovniPartner);
+		Prometnidokument pd = new Prometnidokument(vrstaDokumenta, redniBrojDokumenta, datumFormiranja,
+				datumKnjizenja, statusDokumenta.charValue(), m, pp);
 		pd.save();
 		
 		read();
 	}
 
 	
-	public static void update(Long id, String vrstaDokumenta, Integer redniBrojDokumenta, Date datumFormiranja, Date datumKnjizenja, Character statusDokumenta, Magacin magacin, Poslovnipartner poslovnipartner){
+	public static void update(Long id, String vrstaDokumenta, Integer redniBrojDokumenta, Date datumFormiranja, 
+			Date datumKnjizenja, Character statusDokumenta, Long magacin, Long poslovniPartner){
+		
 			Prometnidokument pd = Prometnidokument.findById(id);
 			pd.vrstaDokumenta = vrstaDokumenta;
 			pd.redniBrojDokumenta = Integer.parseInt(redniBrojDokumenta.toString());
 			pd.datumFormiranja = datumFormiranja;
 			pd.datumKnjizenja = datumKnjizenja;
-			pd.statusDokumenta = statusDokumenta;
-			pd.magacin = magacin;
-			pd.poslovniPartner = poslovnipartner;
+			pd.statusDokumenta = statusDokumenta.charValue();
+			pd.magacin = Magacin.findById(magacin);
+			pd.poslovniPartner = Poslovnipartner.findById(poslovniPartner);
 			
 			pd.save();
 			
