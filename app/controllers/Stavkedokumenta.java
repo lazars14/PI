309d6f26284a9646f5655ffaec533d834a69b32c
zomlaaa95 <zomlaaa95@gmail.com>
2017-10-19 06:25:23 +0200

    Did some validation

--- app/controllers/Stavkedokumenta.java
+++ app/controllers/Stavkedokumenta.java
@@ -15,39 +15,36 @@ import play.mvc.With;
 //@With(Security.class)
 public class Stavkedokumenta extends Controller{
 	
-	public static void read() {
+	public static void create(int kolicina, String cena, String vrednost, Long analitikaMagacinskeKartice, Long roba, Long prometniDokument) {
 		
-		List<Stavkadokumenta> stavkeDok = Stavkadokumenta.findAll();
-		
-		render(stavkeDok);
-	}
-	
-	public static void crete(int kolicina, String cena, String vrednost, Analitikamagacinskekartice analitikamagacinskekartice, Roba roba, Prometnidokument prometnidokument) {
-		
-		Stavkadokumenta stavkadokumenta = new Stavkadokumenta(kolicina, Double.valueOf(cena), Double.valueOf(vrednost), analitikamagacinskekartice, roba, prometnidokument);
+		Analitikamagacinskekartice a = Analitikamagacinskekartice.findById(analitikaMagacinskeKartice);
+		Roba r = Roba.findById(roba);
+		Prometnidokument p = Prometnidokument.findById(prometniDokument);
+		Stavkadokumenta stavkadokumenta = new Stavkadokumenta(kolicina, Double.valueOf(cena), Double.valueOf(vrednost), a, r, p);
 		stavkadokumenta.save();
+		
+		Prometnadokumenta.readAll(stavkadokumenta.prometniDokument.id);
 	}
 	
-	public static void update(Long id, int kolicina, String cena, String vrednost, Analitikamagacinskekartice analitikamagacinskekartice, Roba roba, Prometnidokument prometnidokument) {
+	public static void update(Long id, int kolicina, String cena, String vrednost, Long analitikaMagacinskeKartice, Long roba, Long prometniDokument) {
 		
 		Stavkadokumenta s = Stavkadokumenta.findById(id);
 		s.kolicina = kolicina;
 		s.cena = Double.valueOf(cena);
 		s.vrednost  = Double.valueOf(vrednost);
-		s.analitikaMagacinskeKartice = analitikamagacinskekartice;
-		s.roba = roba;
-		s.prometniDokument = prometnidokument;
+		s.analitikaMagacinskeKartice = Analitikamagacinskekartice.findById(analitikaMagacinskeKartice);
+		s.roba = Roba.findById(roba);
+		s.prometniDokument = Prometnidokument.findById(prometniDokument);
 		s.save();
 		
-		read();
-		
+		Prometnadokumenta.readAll(s.prometniDokument.id);
 	}
 	
 	public static void delete(Long id){
 		Stavkadokumenta stavkadok = Stavkadokumenta.findById(id);
     	stavkadok.delete();
     	
-    	read();
+    	Prometnadokumenta.readAll(stavkadok.prometniDokument.id);
 	}
 
 	public static void filter(){
